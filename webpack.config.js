47a32cd3cc0a92bfd93c6d4567c8efa318793fcf
cokyaman <cokyamanmuhammet@gmail.com>
2021-07-06 18:37:02 +0300

    config code fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,40 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: [
-        "babel-polyfill",
-        "./src/js/index.js"
+  entry: ["babel-polyfill", "./src/js/index.js"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/bundle.js",
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+  },
+  watch: true,
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+      template: path.resolve(__dirname, "src", "index.html"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["babel-preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
-    output: {
-        path: path.resolve(__dirname,"dist"),
-        filename: "js/bundle.js"
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 9000
-    },
-    watch: true,
-    plugins: [
-         new HtmlWebpackPlugin({
-             title: "Development",
-             template: path.resolve(__dirname, "src","index.html")
-         })
-    ],
-    module: {
-        rules : [
-            {
-                test : /\.js$/,
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
-    }
-}
+  },
+};
